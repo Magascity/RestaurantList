a19d6c9f8a166022c2b83db8bccd372c90a3af5c
Ayok Akut <akut.ayok@outlook.com>
2024-09-07 20:29:28 +0100

    Add project files.

---
+++ RestaurantList/Controllers/RestaurantListController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RestaurantList.Data;
+using RestaurantList.Models;
+
+namespace RestaurantList.Controllers
+{
+    public class RestaurantListController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RestaurantListController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        //public async Task<IActionResult> Index()
+        //{
+        //    return View(await _context.Restaurants.ToListAsync());
+        //}
+
+
+        public async Task<IActionResult> Index(string searchString)
+        {
+            var restaurants = from r in _context.Restaurants
+                              select r;
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                restaurants = restaurants.Where(r => r.Name.Contains(searchString));
+                return View(await restaurants.ToListAsync());
+            }
+            return View(await restaurants.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            var restaurant = await _context.Restaurants.Include(rd => rd.RestaurantDishes)
+            .ThenInclude(d => d.Dish)
+            .FirstOrDefaultAsync(x => x.Id == id);
+            if (restaurant == null)
+            {
+                return NotFound();
+            }
+            return View(restaurant);
+        }
+
+    }
+}
---
+++ RestaurantList/Data/ApplicationDbContext.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RestaurantList.Models;
+
+namespace RestaurantList.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<RestaurantDish>().HasKey(rd => new
+            {
+                rd.RestaurantId,
+                rd.DishId
+            });
+
+            modelBuilder.Entity<RestaurantDish>()
+                .HasOne(r => r.Restaurant)
+                .WithMany(rd => rd.RestaurantDishes)
+                .HasForeignKey(r => r.RestaurantId);
+
+
+            modelBuilder.Entity<RestaurantDish>()
+                .HasOne(d => d.Dish)
+                .WithMany(rd => rd.RestaurantDishes)
+                .HasForeignKey(d => d.DishId);
+
+
+            modelBuilder.Entity<Restaurant>().HasData(
+            new Restaurant
+            {
+                 Id = 1,
+                 Name = "Gourmet Pizzeria",
+                 Address = "1234 Culinary St, Flavor, CA 90210",
+                ImageUrl = "https://www.whereyoueat.com/r_gallery_images/rgallery-21635/Best_Italian_Pizza2.jpg"
+            }
+
+            );
+
+            modelBuilder.Entity<Dish>().HasData(
+                new Dish { Id = 1, Name = "Pizza", price = 10 },
+                new Dish { Id = 2, Name = "Pasta", price = 9 }
+            );
+
+            modelBuilder.Entity<RestaurantDish>().HasData(
+                new RestaurantDish { RestaurantId = 1, DishId = 1 },
+                new RestaurantDish { RestaurantId = 1, DishId = 2 }
+            );
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<Dish> Dishes { get; set; }
+        public DbSet<RestaurantDish> RestaurantDishes { get; set; }
+    }
+}
